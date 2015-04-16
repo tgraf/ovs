839129e9234112f826ebd679db40fd35996d84c1
Thomas Graf <tgraf@noironetworks.com>
2015-04-16 17:08:45 +0200

    nat-setup: Cleanup the script

--- nat-setup.sh
+++ nat-setup.sh
@@ -1,72 +1,86 @@
 #!/bin/bash
 
-/usr/share/openvswitch/scripts/ovs-ctl start
-
-ovs-vsctl del-br br0 2> /dev/null
-ovs-vsctl add-br br0
+BR=br0
+GW_IP=1.1.1.1
+GW_MAC="dd:dd:dd:dd:dd:dd"
 
 ################################################################################
 # NAT GATEWAY
 ################################################################################
-ip netns del natns 2> /dev/null
-ip netns add natns
+function setup_nat_gw()
+{
+	ip netns del $1 2> /dev/null
+	ip netns add $1
 
-# interface into namespace
-ovs-vsctl add-port br0 nat-gw -- set Interface nat-gw type=internal
-ip link set nat-gw netns natns
-ip netns exec natns ip link set nat-gw up
-ip netns exec natns ip addr add 10.1.1.2/24 dev nat-gw
-ip netns exec natns ip addr add 10.1.2.2/24 dev nat-gw
+	# interface into namespace
+	ovs-vsctl add-port $BR $1 -- set Interface $1 type=internal
+	ip link set $1 netns $1
+	ip netns exec $1 ip link set $1 up
 
-# route everything back out
-ip netns exec natns ip rule add iif nat-gw lookup 100
-ip netns exec natns ip route add default via 10.1.2.1 table 100
+	# route everything back out
+	ip netns exec $1 ip rule add iif $1 lookup 100
+	ip netns exec $1 ip route add $GW_IP/32 dev $1
+	ip netns exec $1 ip route add default via $GW_IP table 100
 
-ip netns exec natns iptables -t nat -A POSTROUTING -o nat-gw -d 40.0.0.0/8 -j SNAT --to 80.1.1.100
-ip netns exec natns iptables -t nat -A POSTROUTING -o nat-gw -d 50.0.0.0/8 -j SNAT --to 90.1.1.100
-ip netns exec natns iptables -t nat -A POSTROUTING -o nat-gw -d 60.0.0.0/8 -j SNAT --to 100.1.1.100
+	echo "NAT-GW: $1"
+}
 
-################################################################################
-# TENANT 1
-################################################################################
-ip netns del net1 2> /dev/null
-ip netns add net1
-ovs-vsctl add-port br0 p1 -- set Interface p1 type=internal
-ip link set p1 netns net1
-ip netns exec net1 ip link set p1 up
-ip netns exec net1 ip addr add 40.1.1.1/24 dev p1
-ip netns exec net1 ip route add 1.1.1.1/32 dev p1
-ip netns exec net1 ip route add default via 1.1.1.1
+function add_nat()
+{
+	ip netns exec $1 iptables -t nat -A POSTROUTING -o $1 -d $2 -j SNAT --to $3
+
+	echo "SNAT-MAP: dst = $2 => --to-source $3"
+}
 
 ################################################################################
-# TENANT 2
+# TENANTS
 ################################################################################
-ip netns del net2 2> /dev/null
-ip netns add net2
-ovs-vsctl add-port br0 p2 -- set Interface p2 type=internal
-ip link set p2 netns net2
-ip netns exec net2 ip link set p2 up
-ip netns exec net2 ip addr add 50.1.1.1/24 dev p2
-ip netns exec net2 ip route add 1.1.1.1/32 dev p2
-ip netns exec net2 ip route add default via 1.1.1.1 dev p2
+
+function tenant_to_nat_gw()
+{
+	MAC=$(ip netns exec $3 ip link show $3 | grep ether | awk '{print $2}')
+
+	ovs-ofctl add-flow $BR "priority=100,in_port=$1,actions=load:$2->NXM_NX_PKT_MARK[], \
+							mod_dl_dst:$MAC,mod_dl_src:$GW_MAC,output:$(get_ofport $3)"
+			
+}
+
+function do_l3_into_tenant()
+{
+	local MAC=$(ip netns exec $1 ip link show $2 | grep ether | awk '{print $2}')
+
+	ovs-ofctl add-flow $BR "table=2,priority=100,ip,nw_dst=$3,actions= \
+							mod_dl_dst:$MAC, \
+							dec_ttl, \
+							mod_dl_src:$GW_MAC, \
+							output:$(get_ofport $2)"
+}
+
+# NETNS PORT-NAME PREFIX MARK-VALUE NAT_GW
+function setup_tenant()
+{
+	ip netns del $1 2> /dev/null
+	ip netns add $1
+	ovs-vsctl add-port $BR $2 -- set Interface $2 type=internal
+	ip link set $2 netns $1
+	ip netns exec $1 ip link set $2 up
+	ip netns exec $1 ip addr add $3/24 dev $2
+	ip netns exec $1 ip route add $GW_IP/32 dev $2
+	ip netns exec $1 ip route add default via $GW_IP
+
+	tenant_to_nat_gw $(get_ofport $2) $4 $5
+	do_l3_into_tenant $1 $2 $3
+
+	echo "Tenant: $1 port=$2 ip=$3 mark=$4 nat-gw=$5"
+}
 
 ################################################################################
-# TENANT 3
+# OVS BITZ
 ################################################################################
-ip netns del net3 2> /dev/null
-ip netns add net3
-ovs-vsctl add-port br0 p3 -- set Interface p3 type=internal
-ip link set p3 netns net3
-ip netns exec net3 ip link set p3 up
-ip netns exec net3  ip addr add 60.1.1.1/24 dev p3
-ip netns exec net3 ip route add 1.1.1.1/32 dev p3
-ip netns exec net3 ip route add default via 1.1.1.1 dev p3
-
-ovs-ofctl del-flows br0
 
 function get_ofport()
 {
-        local N=$(ovs-ofctl show br0 | grep $1 | cut -d'(' -f1)
+        local N=$(ovs-ofctl show $BR | grep $1 | cut -d'(' -f1)
 
         if [ -z "$N" ]; then
                 exit 1
@@ -88,7 +102,7 @@ function mac2hex()
 
 function arp_responder()
 {
-	ovs-ofctl add-flow br0 "arp, arp_op=1, arp_tpa=$1, \
+	ovs-ofctl add-flow $BR "arp, arp_op=1, arp_tpa=$1, \
 							actions=move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[], \
 							mod_dl_src:$2, \
 							load:2->NXM_OF_ARP_OP[], \
@@ -99,46 +113,35 @@ function arp_responder()
 							in_port"
 }
 
-# Respond to all ARPs
-arp_responder 1.1.1.1 "dd:dd:dd:dd:dd:dd"
-arp_responder 10.1.1.1 "dd:dd:dd:dd:dd:dd"
-arp_responder 10.1.2.1 "dd:dd:dd:dd:dd:dd"
+################################################################################
+# MAIN
+################################################################################
 
-function filter_tenant()
-{
-	MAC=$(ip netns exec natns ip link show nat-gw | grep ether | awk '{print $2}')
-	NAT_IN="mod_dl_dst:$MAC,mod_dl_src:dd:dd:dd:dd:dd:dd,output:$(get_ofport nat-gw)"
-	ovs-ofctl add-flow br0 "priority=100,pkt_mark=0,in_port=$1,actions=load:$2->NXM_NX_PKT_MARK[], \
-						   $NAT_IN"
-}
+/usr/share/openvswitch/scripts/ovs-ctl start
 
-# Mark packets and pass through CT
-filter_tenant $(get_ofport p1) 1
-filter_tenant $(get_ofport p2) 2
-filter_tenant $(get_ofport p3) 3
+ovs-vsctl del-br $BR 2> /dev/null
+ovs-vsctl add-br $BR
 
-ovs-ofctl add-flow br0 "priority=100,in_port=$(get_ofport nat-gw),actions=resubmit(,2)"
-ovs-ofctl add-flow br0 "priority=10,actions=drop"
+setup_nat_gw nat-gw
 
-# GW-MAC PREFIX NEXTHOP
-function do_l3()
-{
-	ovs-ofctl add-flow br0 "table=2,priority=100,ip,nw_dst=$2,actions= \
-							mod_dl_dst:$3, \
-							dec_ttl, \
-							mod_dl_src:$1, \
-							output:$4"
-}
+# SNAT matching DEST-PREFIX
+add_nat nat-gw 40.0.0.0/8 80.1.1.100
+add_nat nat-gw 50.0.0.0/8 90.1.1.100
+add_nat nat-gw 60.0.0.0/8 100.1.1.100
 
-GW_MAC="dd:dd:dd:dd:dd:dd"
-TENANT_MAC=$(ip netns exec net1 ip link show p1 | grep ether | awk '{print $2}')
-do_l3 $GW_MAC 40.1.1.1 $TENANT_MAC $(get_ofport p1)
+ovs-ofctl del-flows $BR
 
-TENANT_MAC=$(ip netns exec net2 ip link show p2 | grep ether | awk '{print $2}')
-do_l3 $GW_MAC 50.1.1.1 $TENANT_MAC $(get_ofport p2)
+arp_responder $GW_IP $GW_MAC
 
-TENANT_MAC=$(ip netns exec net3 ip link show p3 | grep ether | awk '{print $2}')
-do_l3 $GW_MAC 60.1.1.1 $TENANT_MAC $(get_ofport p3)
+# All packets from tenants need to go through NAT-GW, mark packets with
+# tenant specific metadata
+setup_tenant net1 p1 40.1.1.1 1 nat-gw
+setup_tenant net2 p2 50.1.1.1 2 nat-gw
+setup_tenant net3 p3 60.1.1.1 3 nat-gw
+
+# Packets coming out of the NAT box go to table 2
+ovs-ofctl add-flow $BR "priority=100,in_port=$(get_ofport nat-gw),actions=resubmit(,2)"
 
 # Drop everything else
-ovs-ofctl add-flow br0 "table=2,priority=10,actions=drop"
+ovs-ofctl add-flow $BR "priority=10,actions=drop"
+ovs-ofctl add-flow $BR "table=2,priority=10,actions=drop"
